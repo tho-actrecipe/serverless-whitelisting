12e0fc2d250825f80a51f6a9cce135fe2424b3ae
Tho Nguyen <tho.nguyen@asterist.com>
2020-12-11 12:24:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Serverless Whitelisting
+
+Creates a whitelist for IP addresses, CIDR for a serverless application, using serverless resource policies.
+
+## Private and Public Stages
+
+IP addresses, CIDR are whitelisted by stages and `publicPaths`.
+
+- `privateStages`: Private to whitelisted CIDR and IP addresses. In the example below, `staging` and `production` stages are `privateStages`, so only those CIDR and IP addresses can access `staging` and `production`.
+- `publicStages`: No whitelisting necessary. These stages are public to all CIDR and IP addresses.
+- `publicPaths`: No whitelisting necessary regardless stages.
+
+## How to Use
+
+1. Install in your serverless application: `npm install --save serverless-whitelisting`
+2. In your `serverless.yml` file, add the `serverless-whitelisting` to `plugins`, for example:
+   ```
+   plugins:
+   - serverless-whitelisting
+   ```
+3. Within the `provider` block, add a `stage` variable:
+   ```
+   provider:
+     stage: ${opt:stage, 'dev'}
+   ```
+4. Within a `custom` block, add:
+   ```
+   custom:
+     serverless-resource-policy:
+       stage: ${self:provider.stage}
+       privateStages:
+         - staging
+         - production
+       publicStages:
+         - test
+       netblocks:
+         - 212.204.161.60/24
+         - 68.159.185.46
+       publicPaths:
+         - oauth
+         - oauth-test
+   ```
+
+> The `netblocks` object will contain the list of whitelisted IPs.
+
+### Full Example
+
+```
+# serverless.yml
+
+service: my-service-name
+
+plugins:
+  - serverless-whitelisting
+
+provider:
+  stage: ${opt:stage, 'test'}
+
+custom:
+  serverless-resource-policy:
+    stage: ${self:provider.stage}
+    privateStages:
+      - staging
+      - production
+    publicStages:
+      - test
+    netblocks:
+      - 212.204.161.60/24
+      - 68.159.185.46
+    publicPaths:
+      - oauth
+      - oauth-test
+```
---
+++ index.js
@@ -0,0 +1,113 @@
+"use strict";
+
+const semver = require("semver");
+
+const PLUGIN_NAME = "serverless-whitelisting";
+
+const BASE_POLICY = {
+    Effect: "Allow",
+    Principal: "*",
+    Action: "execute-api:Invoke"
+};
+
+const PUBLIC_RESOURCE = {
+    Resource: ["execute-api:/*/*/*"]
+}
+
+class ServerlessPlugin {
+    constructor(serverless, options) {
+        console.log(`Starting serverless-whitelisting`);
+
+        if (!semver.satisfies(serverless.version, ">= 1.12")) {
+            throw new Error(
+                "Requires serverless 1.12 or higher!"
+            );
+        }
+        let config = {};
+        if (serverless.service.custom && serverless.service.custom[PLUGIN_NAME]) {
+            config = serverless.service.custom[PLUGIN_NAME];
+        }
+
+        const hooks = {
+            "before:offline:start": () => this.createResourcePolicy(config, true), // For testing the resource policy
+            "package:initialize": () => this.createResourcePolicy(config)
+        };
+
+        Object.assign(this, {
+            serverless,
+            options,
+            hooks,
+            resourcePolicy: null,
+            provider: serverless.getProvider("aws")
+        });
+    }
+
+    createResourcePolicy(config, inDevMode = false) {
+        if (config && JSON.stringify(config) !== "{}") {
+            const {stage, publicStages, privateStages, netblocks, publicPaths} = config;
+            this.serverless.cli.log(`Creating resource policies for ${stage} stage`);
+
+            // If the currently selected stage is a public stage
+            if (publicStages && publicStages.length && ~publicStages.indexOf(stage)) {
+                this.serverless.cli.log(
+                    `Public Resource policy required for ${stage} stage`
+                );
+                this.resourcePolicy = Object.assign(PUBLIC_RESOURCE, BASE_POLICY);
+            }
+
+            // If the currently selected stage is a private stage
+            if (privateStages && privateStages.length && ~privateStages.indexOf(stage)) {
+                if (!netblocks) {
+                    throw new Error(
+                        `[${PLUGIN_NAME}]: The \`netblocks\` option is required when specifying private stages.`
+                    );
+                }
+
+                let ipRanges = netblocks;
+                if (!ipRanges || ipRanges.length === 0) {
+                    throw new Error(
+                        `[${PLUGIN_NAME}]: Could not determine IP range restriction for ${stage} stage. Please recheck your config.`
+                    );
+                }
+
+                this.serverless.cli.log(
+                    `Private Resource policy required for ${stage} stage`
+                );
+
+                this.resourcePolicy = Object.assign(
+                    {
+                        Condition: {
+                            IpAddress: {
+                                "aws:SourceIp": netblocks
+                            }
+                        }
+                    },
+                    BASE_POLICY
+                );
+            }
+
+            // If set publicPaths
+            if (publicPaths && publicPaths.length) {
+                for (let i = 0; i < publicPaths.length; i++) {
+                    const policy = {
+                        Resource: ["execute-api:/*/*/" + publicPaths[i]]
+                    }
+                    this.resourcePolicy = Object.assign(policy, BASE_POLICY);
+                }
+            }
+
+            // Assign resource policy update
+            const resourcePolicyUpdate = {
+                resourcePolicy: [this.resourcePolicy]
+            };
+
+            if (!inDevMode) {
+                Object.assign(this.serverless.service.provider, resourcePolicyUpdate);
+            } else {
+                this.serverless.cli.log(JSON.stringify(resourcePolicyUpdate));
+            }
+        }
+    }
+}
+
+module.exports = ServerlessPlugin;
